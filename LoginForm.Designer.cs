312c5341e09acbd13364e65bea30de87d6ec6df3
maxim <makr781@ess.ais.ac.nz>
2022-03-22 23:48:09 +1300

    login initial

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.Location = new System.Drawing.Point(106, 111);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(82, 20);
+            this.labelUsername.TabIndex = 0;
+            this.labelUsername.Text = "username";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.Location = new System.Drawing.Point(106, 161);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(100, 23);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "password";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(208, 111);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(146, 22);
+            this.textBoxUsername.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(208, 162);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(146, 22);
+            this.textBoxPassword.TabIndex = 3;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(106, 234);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(248, 29);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Log In";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Location = new System.Drawing.Point(106, 288);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(248, 29);
+            this.buttonRegister.TabIndex = 5;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 377);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Name = "Form1";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonRegister;
+
+        private System.Windows.Forms.Label labelPassword;
+
+        private System.Windows.Forms.Label labelUsername;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Login.Main;
+using Login.Register;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Debug.WriteLine("constructor fired");
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (checkUser(this.textBoxUsername.Text, this.textBoxPassword.Text))
+            {
+                MainForm mf = new MainForm();
+                mf.Show();
+            }
+            else
+            {
+                MessageBox.Show("wrong username or password");
+            }
+        }
+
+        private bool checkUser(string user, string paswd)
+        {
+            try
+            {
+                //"Default": "Server=localhost; Database=<insert db name>; User Id=sa; Password=your_password123"
+                SqlConnection myConnection = new SqlConnection("server=localhost;" + 
+                                                               "Trusted_Connection=yes;" + 
+                                                               "database=LoginAppDB; " + 
+                                                               "connection timeout=30");
+            
+                SqlCommand myCommand = new SqlCommand("SELECT * FROM loginDB.tblUsers", myConnection);
+            
+            
+                SqlDataReader myReader = null;
+                myConnection.Open();
+                myReader = myCommand.ExecuteReader();
+
+                Dictionary<string, string> users = new Dictionary<string, string>();
+                
+                while(myReader.Read())
+                {
+                    var username = myReader["username"].ToString();
+                    var password = myReader["password"].ToString();
+                    users.Add(username,password);
+                }
+                myConnection.Close();
+
+                if (!(users[user] is null) && users[user] == paswd) return true;
+
+            }
+            catch (Exception es)
+            {
+                //MessageBox.Show(es.Message);
+            }
+
+            return false;
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            RegisterForm f2 = new RegisterForm();
+            f2.Show();
+        }
+    }
+}
